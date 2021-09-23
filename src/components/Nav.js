a36d24164b54181a84def74fc697336bca065aaf
IArodriguez99 <73261893+IArodriguez99@users.noreply.github.com>
2021-09-23 10:04:51 -0500

    Nav Component

---
+++ src/components/Nav.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Nav = () => {
+    return (
+        <div>
+            <nav>
+                <h1 className="logo">The Planets</h1>
+                    <ul className = "nav_right">
+                        <li>Mercury</li>
+                        <li>Venus</li>
+                        <li>Earth</li>
+                        <li>Mars</li>
+                        <li>Jupiter</li>
+                        <li>Saturn</li>
+                        <li>Uranus</li>
+                        <li>Neptune</li>
+                   </ul>
+            </nav>
+        </div>
+    )
+}
+
+export default Nav
