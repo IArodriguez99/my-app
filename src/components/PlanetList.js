6a3b84cb8bc9a110b4987f0512c86d18804eb2d8
IArodriguez99 <73261893+IArodriguez99@users.noreply.github.com>
2021-09-23 13:42:43 -0500

    Reusable json config

---
+++ src/components/PlanetList.js
@@ -0,0 +1,17 @@
+const PlanetList = (props) => {
+    
+const planets = props.planets;
+
+    return ( 
+        <div className="planet_list">
+            {planets.map((planet) => (
+                <div className="planet-preview" key={planet.id}>
+                    <h1>{ planet.name }</h1>
+                    <p>{ planet.overview.content }</p>
+                </div>
+            ))}
+        </div>
+     );
+}
+ 
+export default PlanetList;
